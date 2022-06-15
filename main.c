c8691608b54a2b66193aa2d841767b1bb1274df4
Dinislam <101834092+AkenBuliven@users.noreply.github.com>
2022-06-15 15:52:20 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+
+  int a = 0;
+  
+
+  do
+  {
+    scanf ("%d", &a);
+    printf("%d ", a);
+  }
+   
+  while ( a != -9999);
+  {
+  printf ("loh-pidr");
+  }
+    return 0;
+}
+
+  
